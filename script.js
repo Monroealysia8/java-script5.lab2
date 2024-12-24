4d6dae9e16b232c7b9a222b9fdf7f0d1bce78c56
Monroealysia8 <monroealysia8@gmail.com>
2024-12-24 16:22:07 -0600

    first submission

---
+++ script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// Problem 1:
+// Smallest of Three Number
+//  Take three numbers from 3 inputs.
+//  Compare the numbers to find the smallest.
+//  Print the smallest number.
+
+// let a = Number(prompt("Enter a number."));
+// let b = Number(prompt("Enter a second number."));
+// let c = Number(prompt("Enter a third number."));
+//
+// function smallestOf3Nums(a, b, c) {
+//   if (a < b && a < c) {
+// document.write(a);
+//   } else if (b < a && b < c) {
+// document.write(b);
+//   } else {
+// document.write(c);
+//   }
+// }
+//
+// smallestOf3Nums(a, b, c);
+
+//  Problem 2
+// Create a function that takes a number as input and determines if it is positive, negative, or zero.
+// Take a number as input.
+// Check if the number is positive, negative, or zero.
+// Print the result.
+
+//  function operatorOfnumber(number) {
+//   if (number > 0) {
+// document.write("The number is postive.");
+//   } else if (number < 0) {
+// document.write("The number is negative.");
+//   } else {
+// document.write("The number is zero");
+//   }
+// }
+//
+// let number = prompt("Enter a number.");
+// operatorOfnumber (Number(number));
+
+// Problem 3
+// Create a function that takes the user's age as input and determines whether they are eligible to vote. In most countries, the legal voting age is 18 years.
+// Take the user's age as input.
+// Check if the age is 18 or above.
+// Print a message indicating whether the user is eligible to vote.
+
+// function votingRequirements(age) {
+//   if (age >= 18) {
+    // document.write("You are old enough to vote in the U.S.");
+//   } else {
+    // document.write("You are not old enough to vote in the U.S.");
+//   }
+// }
+// 
+// const age = prompt("Enter your age");
+// votingRequirements(Number(age));
